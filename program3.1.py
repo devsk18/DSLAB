cfddc97f28503b41dab6f1787ed40ae18e4431c6
Sam K Thampan <64613068+devsk18@users.noreply.github.com>
2022-09-25 23:39:43 -0700

    Add files via upload

---
+++ program3.1.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+xp = np.array([1,2,4,6])
+yp = np.array([7,8,2,5])
+plt.plot(xp,yp,marker="o",color="red",mec="g",mfc="g",linestyle="dotted")
+plt.show()
